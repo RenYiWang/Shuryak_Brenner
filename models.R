c26d37efc52dad81388aa6b4e52a4903f3832e98
Edward Huang <edwardghg@gmail.com>
2019-03-08 02:19:00 -0800

    Update gitignore and attempt at modified SBE with LET dependence.

--- models.R
+++ models.R
@@ -13,6 +13,13 @@
 # Attribution:  This R script was developed at UC Berkeley. Written by HG pod
 #               and Rainer K. Sachs 2019.
 
+# 1. Need virtual data: start with oversimplified parametrized model; 
+#    add noise and some systematic bias; generate virtual data. Then try to 
+#    calibrate a model and see what the parameters look like.
+# 2. Simulate HG protracted dosing first; eventually use CA pod data to guide simulated data
+# 3. Assume K in MSBE is LET independent
+# 4. Assume most LET dependent quantities in MSBE have standard LET dependence 
+
 # this and next few lines are in R notation 
 # L = LET. 
 # lambda = AR interpreted as a parameter in itself replacing the parameter A 
@@ -27,9 +34,10 @@ rate0 <- 2 * exp(-3); rate <- rate0 / c(1, iL[2:4] / length(L)) # approximates R
 K <- 4 * exp(-3) # from lubdat
 
 
-# proposed parameter ordering: r > t > t0 > q > ep > k > b > a > m
+# proposed parameter ordering: r > l > t > t0 > q > ep > k > b > a > m > adjustable parameters
 
 # r = radiation dose rate
+# l = LET
 # q = dose rate at which 50% of all NTE-susceptible cells become activated
 # ep = equilibrium probability
 # t = time
@@ -44,8 +52,8 @@ equilibrium_probability <- function(r, q) {
   return(1 / (1 + (q / r)))
 }
 
-nte_term <- function(t, ep, k) { 
-  return(ep * t + 1 / (k * ep ^ (-1))) 
+nte_term <- function(t, pe, k) { 
+  return(pe * t + 1 / (k * pe ^ (-1))) 
 }
 
 te_term <- function(r, t) {
@@ -60,7 +68,81 @@ yield <- function(r, t, q, k, b, a, m) { # Mean yield of tumors per individual.
 #================= Modified Shuryak-Brenner Equation (MSBE) ===================#
 modified_yield <- function(r, t, t0, q, k, b, a, m) {
   pe <- equilibrium_probability(r, q)
-  return(b * (1 + a * te_term(r, t) + m * nte_term(t, pe, k) 
-              * (1 - exp(- t / t0)))) # Hazard term
+  return(b * (1 + a * te_term(r, t) + m * nte_term(t, pe, k)) 
+              * (1 - exp(- t / t0))) # Hazard term
 }
 
+# We will need simulated data because no constant flux NSRL data is as yet 
+# available for the very low fluxes where NTE are expected to dominate.
+
+# The general plan will be the following. Choose a version of the Shuryak-Brenner 
+# equation that uses not only dose-rate R and time but also LET as a categorial 
+# predictor variable with 5 values. 
+
+# Modified NTE term with categorical LET dependence
+mod_nte_term <- function(l, t, pe, k, alpha) { # Includes l and alpha.
+  return(alpha * l * nte_term(t, pe, k))
+}
+
+# Modified yield equation with categorical LET dependence (MLSBE)
+LET_yield <- function(r, l, t, t0, q, k, b, a, m, alpha) { # l is for LET, alpha is a parameter.
+  pe <- equilibrium_probability(r, q)
+  return(b * (1 + a * te_term(r, t) + m * mod_nte_term(l, t, pe, k, alpha)) 
+              * (1 - exp(- t / t0)))
+}
+
+# Choose reasonable numerics so that the equation can produce many simulated 
+# data points Add some random noise and a systematic bias to the simulated 
+# data points. Now devise a similar but not identical version of the 
+# Shuryak-Brenner equation and don't fill in all the numerics -- leave in some 
+# unknown adjustable parameters. Calibrate these from the simulated noise-added
+# bias-added data points. That gives an HZE effect equation with flux and time
+# as predictor variables. 
+
+## EGH: assume chosen LET values are 25, 70, 193, 250, 464
+## Plotting set up
+library(colorspace)
+palette <- c("orange", "red", "purple", "blue", "green") # Color palette for LET ions
+alt_palette <- rev(heat_hcl(5, h = c(0, -100), c = c(40, 80), l = c(75, 40), power = 3)) 
+LET <- c(25, 60, 193, 250, 464) # Ne, Si, Fe 6, Fe 3, Nb
+
+## Figure 0: Noisy plots of MLSBE with arbitrary parameter settings
+plot(NULL, xlim = c(0, 100), ylim = c(0, 10000),
+     ylab = "Tumor yield count", xlab = "Dose rate (cGy/day)")
+for (i in 1:length(LET)) {
+  points(1:100, LET_yield(r=0.01 * 1:100, l=LET[i], # dose rate up to 1 cGy/day,
+                          # LET taken from values set earlier.
+                          t=365, t0=14, q=0.5, # Over a year, saturation point 
+                          # is two weeks, 0.5 cGy/day is point in which 
+                          # 50% of susceptible cells are activated.
+                          k=2, b=2, a=1, m=10, alpha = 0.004) # k, b, a, m, and
+                          # alpha chosen arbitrarily.
+         + rnorm(100, mean=0, sd=400), # Assumes Gaussian, second argument 
+         # can bias the noise.
+         col = palette[i])
+}
+
+
+# Repeat four times for the 
+# remaining LET values. 
+
+
+# Choose the LET-independent SLI effect equation effect = alpha * flux * time 
+# where alpha is the only adjustable parameter. Generate
+# noisy SLI data data= alpha*[flux +C* flux^2]*time by choosing a numerical 
+# value for flux and then adjusting C so the quadratic component is, say, 1/10 
+# the linear component and then adding noise.
+
+# sli_data <- alpha * (flux + c * flux ^ 2) * time
+
+# Now we have six effect equations, 5 HZE labelled by LET and one SLI. 5 have 
+# adjustable parameters and differ only by having different LETs. The other has 
+# adjustable parameter alpha. We have six corresponding data sets. Now consider 
+# mixtures, e.g. of two HZE ions or of more than two ions. Combine these using 
+# incremental effect additivity, which should now be easy since the damage is in 
+# fact arriving incrementally.
+
+# Draw some figures, write some paragraphs, publish, devise some jokes to use 
+# when we team-tag our talk accepting our joint award of the Gray medal at the 
+# 2021 International Radiation Research Society meeting. Celebrate at the 
+# I-house with lots of Lox.
